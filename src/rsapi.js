5c7d0dab37cc40038e0133049ec3eeaec2aeb722
RKCODE-Lab <73872225+RKCODE-Lab@users.noreply.github.com>
2025-06-27 16:39:10 +0530

    Update rsapi.js
    Updated the URL's to access the Front-end of the app

--- src/rsapi.js
+++ src/rsapi.js
@@ -1,32 +1,29 @@
 import axios from 'axios';
-//const baseURL = window.location.origin + '/QuaLIS'
+const baseURL = window.location.origin + '/QuaLIS'
 //Uncomment below for java 8 and comment for Java 21
 //const baseURL = 'http://localhost:7072/QuaLIS';
 //Comment below for Java 8 and uncomment for Java 21
-const baseURL = 'http://localhost:6063/QuaLIS';
+//const baseURL = 'http://localhost:6063/QuaLIS';
 //const baseURL = 'http://localhost:8010/QuaLIS';
 export default axios.create({
   baseURL: baseURL
 });
 
 export const serverUrl = () => {
-
   return baseURL
-
 }
 
 export const clientUrl = () => {
   return baseURL
-
 }
 
 export const fileViewUrl = () => {
-  return 'http://localhost:9091'
-   //return window.location.origin
+  //return 'http://localhost:9091'
+   return window.location.origin
 }
 
 export const reportUrl = () => {
-  return 'http://localhost:9091' + '/JavaReportingTool/';
+  //return 'http://localhost:9091' + '/JavaReportingTool/';
   //return 'http://localhost:9090';
-  // return window.location.origin + '/JavaReportingTool/';
+  return window.location.origin + '/JavaReportingTool/';
 }
